d41be0c4347c6948db92c2a7d1642c9df9e9923e
Carlos Gochez <carlos.gochez@unitec.edu>
2013-01-17 09:16:48 -0600

    Agregue unos set y get

--- src/Test/TestClass.java
+++ src/Test/TestClass.java
@@ -16,6 +16,23 @@ public class TestClass {
         this.codigo = codigo;
         this.nombre = nombre;
     }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
     
     
 }
