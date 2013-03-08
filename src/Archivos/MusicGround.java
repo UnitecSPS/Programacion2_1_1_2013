3a58ad0ea6e219e5727b48d59de94d793c7d3452
Carlos Gochez <carlos.gochez@unitec.edu>
2013-03-07 22:29:45 -0600

    Detalle de la TG3

--- src/Archivos/MusicGround.java
+++ src/Archivos/MusicGround.java
@@ -17,9 +17,11 @@ public class MusicGround {
         Scanner lea = new Scanner(System.in);
         
         do{
+            System.out.println("\n\nMENU PRINCIPAL\n---------");
             System.out.println("1- Agregar Cancion");
             System.out.println("2- Listar Canciones dispo.");
-            System.out.println("3- Salir");
+            System.out.println("3- Bajar Cancion");
+            System.out.println("4- Salir");
             System.out.print("\nEscoja Opcion: ");
             op = lea.nextInt();
             
@@ -35,12 +37,23 @@ public class MusicGround {
                     case 2:
                         roll.songList();
                         break;
+                    case 3:
+                        System.out.println("Nombre Cliente: ");
+                        String c = lea.next();
+                        System.out.println("Cod Cancion: ");
+                        int cod = lea.nextInt();
+                        
+                        if( roll.download(cod, c))
+                            System.out.println("Se bajo masiso!");
+                        else
+                            System.out.println("No Existe o no esta Disponible");
+                        break;
                 }
             }
             catch(Exception e){
                 System.out.println("Error: " + e.getMessage());
             }
-        }while(op!=3);
+        }while(op!=4);
         
     }
 }
--- src/Archivos/Rolas.java
+++ src/Archivos/Rolas.java
@@ -6,6 +6,7 @@ package Archivos;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Date;
 
 /**
  *
@@ -72,7 +73,7 @@ public class Rolas {
     
     public void songList()throws IOException{
         rSongs.seek(0);
-        System.out.println("LISTADO DISPONIBLE DE CANCIONES");
+        System.out.println("\nLISTADO DISPONIBLE DE CANCIONES");
         System.out.println("-------------------------------");
         
         while( rSongs.getFilePointer() < rSongs.length() ){
@@ -90,4 +91,107 @@ public class Rolas {
             }
         }
     }
+    
+    public boolean search(int codsong)throws IOException{
+        rSongs.seek(0);
+        
+        while(rSongs.getFilePointer() < rSongs.length() ){
+            int cod = rSongs.readInt();
+            
+            long pos = rSongs.getFilePointer();
+            rSongs.readUTF();
+            rSongs.seek( rSongs.getFilePointer() + 20 );
+            boolean dispo = rSongs.readBoolean();
+            
+            if( dispo && codsong == cod){
+                rSongs.seek(pos);
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean download(int codsong, String cliente)throws IOException{
+        if( search(codsong) ){
+            System.out.println("Bajando " + rSongs.readUTF() + " .... ");
+            double prec = rSongs.readDouble();
+            
+            //actualizar bajadas
+            int cantb = rSongs.readInt();
+            rSongs.seek(rSongs.getFilePointer()-4);
+            rSongs.writeInt(cantb + 1);
+            
+            ///crear factura
+            createInvoice(codsong, prec, cliente);
+            return true;
+        }
+        return false;
+    }
+
+    private void createInvoice(int codsong, double prec, String c)throws IOException {
+        rInvoices.seek( rInvoices.length() );
+        //cod fact
+        rInvoices.writeInt( getNewCodeInvoice() );
+        //cod song
+        rInvoices.writeInt(codsong);
+        //cliente
+        rInvoices.writeUTF(c);
+        //precio
+        rInvoices.writeDouble(prec);
+        //fecha
+        rInvoices.writeLong(new Date().getTime());
+    }
+    
+    //TODO: TG3
+    /**
+     * Debe de registrar un nuevo review a una cancion existente y
+     * disponible.(20%)
+     * @param codsong Codigo de la cancion
+     * @param stars Estrellas asignadas (1-5)
+     */
+    public void rateSong(int codsong, int stars){
+        
+    }
+    
+    /**
+     * Debe desactivar una cancion existente segun el codigo de la misma (10%)
+     * @param codsong Codigo de la cancion
+     */
+    public void deactivateSong(int codsong){
+        
+    }
+    
+    /**
+     * Debe mostrar TODOS los datos de una factura segun su codigo (20%)
+     * @param codinvoice Cod Invoice
+     */
+    public void showInvoice(int codinvoice){
+        //Se muestra:
+        //Fecha en la que fue bajada la cancion
+        //Cod Factura // Cod Cancion - Nombre de Cancion
+        // Cliente // Precio de la factura
+    }
+    
+    /**
+     * Debe mostrar TODOS los datos de la cancion que MAS veces se ha
+     * bajado, se muestra tambien cuantas veces se ha bajado (20%)
+     */
+    public void topSong(){
+        
+    }
+    
+    /**
+     * Debe de retornar el total que ha ganado la empresa con las canciones
+     * bajadas desde una fecha min hasta el presente. (20%)
+     * @param min La fecha de inicio del calculo, la factura debe de haber
+     *      sido hecha en >= dicha fecha.
+     * @return El monto ganado.
+     */ 
+    public double profitsSince(Date min){
+        return 0;
+    }
+    
+    //TODO= Agregar las opciones en el Main para probarlas (10%)
+    
 }
